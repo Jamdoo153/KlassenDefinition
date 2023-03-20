3086cd1d6af797482d696577c0574a4a0fc8a349
Michael Walke <Jamdoo@gmx.de>
2023-03-20 20:50:20 +0100

    Erst initalisierung

---
+++ Auto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlassenDefinieren
+{
+    internal class Auto
+    {
+        public string marke;
+        public int bauJahr;
+
+         public void Beschleunigung()
+         {
+            if (istSchnell())
+            {
+               Console.WriteLine("Habe schnell beschleunigung Year");
+            }
+            else
+            {
+               Console.WriteLine("Habe normal beschleunigt ööööh");
+            }
+            }
+            private bool istSchnell()
+            {
+                if (marke == "Porsche")
+                {
+                    return true;
+                }
+                return false;
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace KlassenDefinieren
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Auto MyCar = new Auto();
+            Auto YouCar = new Auto();
+            Auto ChiefCar = new Auto();
+            MyCar.marke = "VW";
+            MyCar.bauJahr = 13;
+            YouCar.marke = "Audi";
+            YouCar.bauJahr = 8;
+            ChiefCar.marke = "Porsche";
+            ChiefCar.bauJahr = 1;
+
+            MyCar.Beschleunigung();
+            YouCar.Beschleunigung();
+            ChiefCar.Beschleunigung();
+            Console.ReadKey();
+        }
+    }
+}
